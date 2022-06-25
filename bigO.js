ad1de82f3783fa69ab868213d828fb5e9261f1d4
jscodeguy <im.still.at.school99@gmail.com>
2022-06-25 15:37:28 -0400

    used array fill to test performance times and efficiency

--- bigO.js
+++ bigO.js
@@ -1,11 +1,15 @@
 const nemo = ['nemo']
-
+const everyone = ["dory", "bruce", "marlin", "nemo", "gill", "bloat", "nigel", "squirt", "darla", "hank"]
+const large = new Array(1000000).fill('nemo')
 const findNemo = (array) => {
+    let t0 = performance.now()
     for (let i = 0; i < array.length; i++) {
         if (array[i] === 'nemo'){
             console.log('found nemo')
         }
     }
+    let t1 = performance.now()
+    console.log('call to find nemo took ' + (t1-t0) + ' milliseconds')
 }
 
-findNemo(nemo)
+findNemo(large)
